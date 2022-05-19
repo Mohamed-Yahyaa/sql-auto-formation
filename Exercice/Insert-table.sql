89ec73f856cbcd16b119ce321ba360e16a92ae74
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-05-19 12:02:32 +0100

    Insert table add

---
+++ Exercice/Insert-table.sql
@@ -0,0 +1,12 @@
+INSERT INTO departements(DNO,DNOM,DIR,VILLE)
+VALUES (1,'Commercial',30,'New York') ,
+ (2,'Production',20,'Houston') , 
+  (3,'Développement',40,'Boston') ;
+
+
+
+INSERT INTO Employes(ENO,ENOM,PROF,DATEEMB,SAL,COMM,DNO)
+VALUES (10,'Joe','Ingénieur','1.10.1993',4000,3000,3) , 
+(20,'Jack','Technicien','1.5.1988',3000,2000,2) ,
+ (30,'Jim','Vendeur','1.3.1980',5000,5000,1) , 
+ (40,'Lucy','Ingénieur','1.3.1980',5000,5000,3) ;
